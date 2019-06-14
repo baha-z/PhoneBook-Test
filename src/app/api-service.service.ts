dcc3c4b47afe2c88bb3171a69a3069b198760bca
Héctor Vargas <hector.vargas.infante@gmail.com>
2019-06-14 11:02:12 -0400

    basic service and person filter list

---
+++ src/app/api-service.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServiceService {
+  url = 'http://private-anon-40ba9623e1-testphonebook.apiary-mock.com';
+
+  constructor(private http: HttpClient) { }
+
+  searchData(title: string): Observable<any> {
+    return this.http.get(`${this.url}?s=${encodeURI(title)}`).pipe(
+      map(results => results['Search'])
+    );
+  }
+
+  getPersona() {
+    return this.http.get(`${this.url}/persona`);
+  }
+
+  getRegion() {
+    return this.http.get(`${this.url}/region`).subscribe((res)=>{
+      console.log(res);
+    }); 
+  }
+
+  
+
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { ApiServiceService } from '../api-service.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit  {
 
-  constructor() {}
+  public personList = [];
+  public filteredArray = [];
+  public name: string;
+  public lastname: string;
+
+  constructor(
+    private apiService: ApiServiceService
+  ) {}
+
+   
+  ngOnInit() {
+    this.getPersona();
+   }
+
+  getPersona() {
+   this.apiService.getPersona().subscribe(
+    (data: any) => { // Success
+      console.log(data);
+      data.map((person: any) =>{
+        if (person.activo){
+          this.personList.push(person);
+        }
+      });       
+    },
+    (error) =>{
+      console.error(error);
+    }
+  )
+  }
+
+  filterItems() {
+    this.filteredArray = this.personList;
+    if (this.name !== "" && this.name !== undefined){
+      this.filteredArray = this.filteredArray.filter(item => 
+        item.nombre.toLowerCase().indexOf(this.name.toLowerCase()) > -1
+      );
+    }
+    if (this.lastname !== "" && this.lastname !== undefined){
+      this.filteredArray = this.filteredArray.filter(item => 
+        item.apellido.toLowerCase().indexOf(this.lastname.toLowerCase()) > -1
+      );
+    }
+    console.log(this.filteredArray);
+  }
 
 }
